e6da3ce1e5dc9b5e9d56bd9c52826f51f2e4aa15
Guilherme Vilabril <vilabril@gmail.com>
2021-06-24 00:46:18 -0300

    Service layer, component registration

---
+++ src/main/java/com/vilabril/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.vilabril.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vilabril.course.entities.User;
+import com.vilabril.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long Id) {
+		Optional<User> obj = repository.findById(Id);
+		return obj.get();
+		
+	}
+
+}
