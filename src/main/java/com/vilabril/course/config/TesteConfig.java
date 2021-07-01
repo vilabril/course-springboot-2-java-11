1c48876a2ed4b59ea4e8a8d82237713d4b75d64b
Guilherme Vilabril <vilabril@gmail.com>
2021-07-01 00:55:06 -0300

    Payment, one-to-one association

--- src/main/java/com/vilabril/course/config/TesteConfig.java
+++ src/main/java/com/vilabril/course/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.vilabril.course.entities.Category;
 import com.vilabril.course.entities.Order;
 import com.vilabril.course.entities.OrderItem;
+import com.vilabril.course.entities.Payment;
 import com.vilabril.course.entities.Product;
 import com.vilabril.course.entities.User;
 import com.vilabril.course.entities.enums.OrderStatus;
@@ -63,6 +64,8 @@ public class TesteConfig implements CommandLineRunner{
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		
 				
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
@@ -83,6 +86,10 @@ public class TesteConfig implements CommandLineRunner{
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);// a class order vai atribuir o valor
+		orderRepository.save(o1); // chama novamente a order para salvar novamente o pedido como pagamento
 		
 	}
 
