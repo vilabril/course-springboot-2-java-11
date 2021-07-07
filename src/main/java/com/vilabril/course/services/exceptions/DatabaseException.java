64ecee04ff4a3d4ba77582e8b5fd7c45b8d16575
Guilherme Vilabril <vilabril@gmail.com>
2021-07-07 00:28:15 -0300

    Exception handling - delete

---
+++ src/main/java/com/vilabril/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.vilabril.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+
+}
